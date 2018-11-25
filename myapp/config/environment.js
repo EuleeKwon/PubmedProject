7fa9987d32dd86f386e19fd2aee93e3885744c6c
euleeKwon <leabinia@naver.com>
2018-11-26 02:08:32 +0900

    changewhole

--- myapp/config/environment.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT:3306,
-    SERVICEKEY:"",
-    API_URL:"http://openapi.epis.or.kr/openapi/service/PcInfoService/getFrmprdPrdlstPcList",
-    PAGINATION:{
-        PAGE_SIZE:10
-    }
-};
